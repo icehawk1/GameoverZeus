03276326a4c038bc1a0b888e17308cc2a25b73b3
Martin Haug <martinhaug@smart-mail.de>
2016-06-20 14:26:13 +0100

    + Grüner Testcase um einen HTTP-Server zu starten und eine seite abzurufen.

--- GameoverTopology.py
+++ GameoverTopology.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import random
 from mininet.log import setLogLevel
 from mininet.topo import Topo
 
@@ -10,29 +11,53 @@ class GameoverTopology(Topo):
     num_cncservers = 2
 
     switch = None
-    botlist = list()
-    proxylist = list()
-    cncserverlist = list()
+    botdict = dict()
+    proxydict = dict()
+    cncserverdict = dict()
 
     def build(self, n=2):
         """Single switch connected to n hosts."""
         self.switch = self.addSwitch('switch1')
 
-        for i in range(self.num_bots):
-            bot = self.addHost("bot%s" % i)
-            self.botlist += bot
-            self.addLink(bot, self.switch)
+        for i in range(self.num_cncservers):
+            cncserver = self.addHost("cnc%s" % i)
+            self.cncserverdict[cncserver] = CnCServer(cncserver)
+            self.addLink(cncserver, self.switch)
 
         for i in range(self.num_proxies):
             proxy = self.addHost("proxy%s" % i)
-            self.proxylist += proxy
+            self.proxydict[proxy] = Proxy(proxy, random.choice(self.cncserverdict.keys()))
             self.addLink(proxy, self.switch)
 
-        for i in range(self.num_cncservers):
-            cncserver = self.addHost("cnc%s" % i)
-            self.cncserverlist += cncserver
-            self.addLink(cncserver, self.switch)
+        for i in range(self.num_bots):
+            bot = self.addHost("bot%s" % i)
+            self.botdict[bot] = Bot(bot, random.choice(self.proxydict.keys()))
+            self.addLink(bot, self.switch)
 
+
+class Bot:
+    def __init__(self, name, proxy):
+        """
+        :type proxy: str
+        """
+        assert isinstance(name, str)
+        assert isinstance(proxy, str)
+        self.name = name
+        self.proxy = proxy
+
+
+class Proxy:
+    def __init__(self, name, cncserver):
+        assert isinstance(name, str)
+        assert isinstance(cncserver, str)
+        self.name = name
+        self.cncserver = cncserver
+
+
+class CnCServer:
+    def __init__(self, name):
+        assert isinstance(name, str)
+        self.name = name
 
 if __name__ == '__main__':
     # Tell mininet to print useful information
--- GameoverTopologyTest.py
+++ GameoverTopologyTest.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2.7
+import time
 import unittest
 from mininet.net import Mininet
 
@@ -17,16 +18,32 @@ class GameoverTopologyTest(unittest.TestCase):
     def tearDownClass(cls):
         cls.net.stop()
 
-    # @unittest.skip("go away")
     def testPingAll(self):
         """test if pingAll succeeds without packet loss"""
         packet_loss = self.net.pingAll()
         self.assertEquals(0, packet_loss)
 
+    @unittest.skip("takes a long time")
     def testPingPair(self):
         """test if pingPair succeeds without packet loss"""
         packet_loss = self.net.pingPair()
         self.assertEquals(0, packet_loss)
+
+    def testConnectToHttpServer(self):
+        cncserver = self.net.get(random.choice(self.topo.cncserverdict.keys()))
+        cncserver.cmd('python -m SimpleHTTPServer 8000 &')
+        time.sleep(5)
+
+        bot = self.net.get(random.choice(self.topo.botdict.keys()))
+        wgetoutput = bot.cmd("wget -O - %s:8000" % cncserver.IP())
+        print wgetoutput
+        self.assertTrue("200 OK" in wgetoutput)
+        self.assertTrue("Directory listing for /" in wgetoutput)
+
+        cncserver.cmd("kill %python")
+
+    def testCommunicationFromBotToCnCServer(self):
+        pass
 
 
 if __name__ == '__main__':

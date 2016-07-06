1458810aac838b41dc405b230a6dd2a4901bcffd
Martin Haug <martinhaug@smart-mail.de>
2016-07-06 10:19:18 +0100

    + Corrected basedir in emu_config

--- MainTestsuite.py
+++ MainTestsuite.py
@@ -8,11 +8,12 @@ import unittest
 
 from resources import emu_config
 
-logging.basicConfig(format="%(threadName)s: %(message)s", level=logging.INFO)
-os.environ["DEBUSSY"] = "1"
+if __name__ == '__main__':
+    logging.basicConfig(format="%(threadName)s: %(message)s", level=logging.DEBUG)
+    os.environ["DEBUSSY"] = "1"
 
-testSuite = unittest.TestSuite()
-testloader = unittest.TestLoader()
-testSuite.addTests(testloader.discover(emu_config.basedir, pattern="*Test.py"))
-testrunner = unittest.TextTestRunner()
-testrunner.run(testSuite)
+    testSuite = unittest.TestSuite()
+    testloader = unittest.TestLoader()
+    testSuite.addTests(testloader.discover(emu_config.basedir, pattern="*Test.py"))
+    testrunner = unittest.TextTestRunner()
+    testrunner.run(testSuite)
--- resources/emu_config.py
+++ resources/emu_config.py
@@ -3,5 +3,6 @@
 import os
 
 basedir = os.path.dirname(os.path.realpath(__file__))  #: The directory where the python code of this emulator is stored
+basedir = os.path.join(basedir, os.pardir)  # Start path should be the parent of this files directory
 PORT = 8080  #: The port CnC Servers and proxies listen on per default
 SOCKET_DIR = "/tmp/overlordsocket/"

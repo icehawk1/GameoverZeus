8f8ded60b68c4349b9539789a286e2e3e29e7d51
Martin Haug <martinhaug@smart-mail.de>
2016-08-11 15:40:13 +0100

    + install script works on university servers

--- install-stuff.sh
+++ install-stuff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Install gcc and virtualbox guest additions first
 apt-get update
-apt-get install floodlight git mininet iptables wireshark nmap openssh-server goldeneye pip d-itg curl wget\
-                python-matplotlib python-netifaces
+apt-get install --ignore-missing -y gcc floodlight git mininet iptables wireshark tshark d-itg curl wget git tcptrace\
+                python-pip python-thrift thrift-compiler goldeneye pip siege
 wget https://atom.io/download/deb -O atom.deb
 gdebi atom.deb
-pip2 install kademlia rfc3987
+pip2 install kademlia rfc3987 marshmallow validators twisted tornado netifaces matplotlib
 apt-get upgrade

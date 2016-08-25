db99b74f6d5dfab0c3ccb780abb2cb4389cef960
Martin Haug <martinhaug@smart-mail.de>
2016-08-25 14:53:09 +0100

    + PingExperiment refactored, so dass es von BriteExperiment erbt. Vorher war es ein simples script.

--- PingExperiment.py
+++ PingExperiment.py
@@ -3,63 +3,81 @@
 import os, time, logging, sys, json, random
 from datetime import datetime
 
-sys.path.append(os.path.dirname(__file__))
-from mininet.cli import CLI
-
-from resources import emu_config
-from utils.MiscUtils import datetimeToEpoch
+from resources.emu_config import logging_config, PORT
 from utils.TcptraceParser import TcptraceParser
+from Experiment import BriteExperiment
+from utils.MiscUtils import mkdir_p, datetimeToEpoch
+
+
+class PingExperiment(BriteExperiment):
+    def __init__(self):
+        super(PingExperiment, self).__init__()
+        self.pcapfile = os.path.join(self.outputdir, "tcptrace/victim.pcap")
+        mkdir_p(os.path.dirname(self.pcapfile))
+
+    def _setup(self):
+        super(PingExperiment, self)._setup()
+
+        nodes = set(self.topology.nodes)
+        assert len(nodes) >= 28
+        self.setNodes("servents", set(random.sample(nodes, 10)))
+        nodes -= self.getNodes("servents")
+        self.setNodes("clients", set(random.sample(nodes, 15)))
+        nodes -= self.getNodes("clients")
+        self.setNodes("victim", set(random.sample(nodes, 1)))
+        nodes -= self.getNodes("victim")
+        self.setNodes("sensor", set(random.sample(nodes, 1)))
+        self.setNodes("bots", self.getNodes("servents") | self.getNodes("clients"))
+
+    def _start(self):
+        super(PingExperiment, self)._start()
+
+        pingresult = self.mininet.pingPair()
+        logging.debug("pingpair: %s"%pingresult)
+
+        assert len(self.getNodes("victim")) >= 1
+        # Get a random element from a set. random.choice() does not work here.
+        victim = random.sample(self.getNodes("victim"), 1)[0]
+        logging.debug("IP of Victim: %s"%(victim.IP()))
+        # Start the necessary runnables
+        self.overlord.startRunnable("Victim", "Victim", hostlist=[h.name for h in self.getNodes("victim")])
+        self.overlord.startRunnable("Sensor", "Sensor", {"pagesToWatch": ["http://%s:%d/?root=1234"%(victim.IP(), PORT)]},
+                                    hostlist=[h.name for h in self.getNodes("sensor")])
+        for h in self.getNodes("servents"):
+            peerlist = random.sample([peer.IP() for peer in self.getNodes("servents") if not peer == h], 3)
+            self.overlord.startRunnable("ping.Servent", "Servent", {"peerlist": peerlist, "pauseBetweenDuties": 5},
+                                        hostlist=[h.name])
+        for h in self.getNodes("clients"):
+            peerlist = random.sample([peer.IP() for peer in self.getNodes("servents") if not peer == h], 3)
+            self.overlord.startRunnable("ping.Client", "Client", {"peerlist": peerlist, "pauseBetweenDuties": 5},
+                                        hostlist=[h.name])
+
+        chosenOne = random.sample(self.getNodes("servents"), 1)[0]
+        kwargsStr = json.dumps({"url": "http://%s:%d/ddos_me"%(victim.IP(), PORT)})
+        curlcmd = "timeout 60s wget -q -O - --post-data 'command=ddos_server&kwargs=%s&timestamp=%d' '%s'"%(
+            kwargsStr, datetimeToEpoch(datetime.now()), "http://%s:%d/current_command"%(chosenOne.IP(), PORT))
+        result = chosenOne.cmd(curlcmd, verbose=True)
+        assert result.strip() == "OK", "Could not send the DDoS-command to the bot %s: |%s|"%(chosenOne, result)
+
+        victim.cmd(self.tsharkCommand%self.pcapfile)
+        logging.debug("Runnables wurden gestartet")
+        time.sleep(25)
+
+    def _executeStep(self, num):
+        result = super(PingExperiment, self)._executeStep(num)
+        return result
+
+    def _stop(self):
+        print "jobs servent: ", random.sample(self.getNodes("servents"), 1)[0].cmd("jobs")
+        print "jobs victim: ", random.sample(self.getNodes("victim"), 1)[0].cmd("jobs")
+        super(PingExperiment, self)._stop()
+
+    def _produceOutputFiles(self):
+        ttparser = TcptraceParser()
+        stats = ttparser.plotConnectionStatisticsFromPcap(self.pcapfile)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(**emu_config.logging_config)
-    cleanup()
-    mininet, overlord, switch = initMininet()
-
-    # Create all the hosts that make up the experimental network and assign them to groups
-    servents = {addHostToMininet(mininet, switch, "servent%d"%i, overlord, bw=25) for i in range(10)}
-    nonInfectedHosts = set()
-    victim = addHostToMininet(mininet, switch, "victim%d"%1, overlord, bw=125)
-    sensor = addHostToMininet(mininet, switch, "sensor%d"%1, overlord)
-    bots = lambda: (servents)
-    nodes = lambda: (bots() | {victim, sensor} | nonInfectedHosts)
-    assert len(nodes()) > 0
-    assert len(bots()) > 0 and len(servents) <= len(nodes())
-
-    victimAddress = (victim.IP(), emu_config.PORT)
-    startMininet(mininet, nodes())
-
-    for h in servents:
-        peerlist = random.sample([peer.IP() for peer in servents if not peer == h], 3)
-        overlord.startRunnable("ping.Servent", "Servent", {"peerlist": peerlist, "pauseBetweenDuties": 5},
-                               hostlist=[h.name])
-    overlord.startRunnable("Victim", "Victim", {}, hostlist=[victim.name])
-    overlord.startRunnable("Sensor", "Sensor", {"pagesToWatch": ["http://%s:%d/?root=1432"%victimAddress]},
-                           hostlist=[sensor.name])
-    time.sleep(5)
-
-    chosenOne = random.sample(servents, 1)[0]
-    curlcmd = "timeout 60s wget -q -O - --post-data 'command=ddos_server&kwargs=%s&timestamp=%d' '%s'"%(
-        json.dumps({"url": "http://%s:%d/ddos_me"%victimAddress}), datetimeToEpoch(datetime.now()),
-        "http://%s:%d/current_command"%(chosenOne.IP(), emu_config.PORT))
-    result = chosenOne.cmd(curlcmd, verbose=True)
-    #print chosenOne.cmd("echo $?")
-    logging.info("wget: %s"%result)
-    time.sleep(2)
-
-    time.sleep(25)
-    for i, prob in enumerate([0.3, 0.2, 1]):
-        print "desinfect %d"%i
-        desinfected = set(overlord.desinfectRandomBots(prob, [h.name for h in bots()]))
-        servents -= desinfected
-        nonInfectedHosts |= desinfected
-        print bots()
-        time.sleep(15)
-    time.sleep(10)
-
-    print "servent: ", chosenOne.cmd("jobs")
-    print "victim: ", victim.cmd("jobs")
-
-    #CLI(mininet)
-    overlord.stopEverything()
-    mininet.stop()
+    logging.basicConfig(**logging_config)
+    experiment = PingExperiment()
+    experiment.executeExperiment()
